013cdfd2ca7c38ff0da3ebc8aa89d69ac72ff139
Moody Rahman <moody.byte@gmail.com>
2022-02-05 00:00:23 -0500

    mmmmm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
+function Cell ({xcoor, ycoor, board, setBoard}) {
+  const [state, setState] = useState(0);
+  const x = xcoor
+  const y = ycoor
+
+  const handleClick = (e) => {
+    console.log(x, y)
+    let curr = state === 0 ? 1 : 0
+    setState(curr);
+
+    setBoard(board.map((row, xi) => row.map((col, yi) => xi===x&&yi===y?curr:col))) 
+  }
+
+
+  return <div onClick={handleClick} style={{background:state===0?"white":"black", height:"50px"}}> {state} </div>
+}
+
+
+
 function App() {
+  const [board, setBoard] = useState(Array(6).fill(0).map(x => Array(6).fill(0)))
+  let next = Array(6).fill(0).map(x => Array(6).fill(0))
+
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:"grid", gridTemplateColumns:"repeat(6, 1fr)"}}>
+      {board.map((x, xi) => {
+        return <>{x.map((y, yi) => {
+          return <Cell xcoor={xi} ycoor={yi} 
+          board={board} setBoard={setBoard} // really ought be a context ngl 
+          />
+        })} </>
+      })}
+
+
+      <br />
+      <button onClick={(e) => {console.log(board)}}> mmm </button>
     </div>
   );
 }
